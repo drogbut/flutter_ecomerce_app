cc6066d7a9b9c247e8a45458befe96ce0c7622ab
Drogbut <tchioidriss@yahoo.fr>
2024-03-20 22:05:52 +0100

    init projet with git

---
+++ lib/my_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'my_home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter folders structure',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter folders structure'),
+    );
+  }
+}

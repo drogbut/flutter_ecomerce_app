6dabf00301aec7ef4c7561e228ec3c6a693f94a1
Drogbut <tchioidriss@yahoo.fr>
2024-06-16 14:48:52 +0200

    Add core/constants/my_styles.dart - Define the global app style

---
+++ lib/core/constants/my_styles.dart
@@ -0,0 +1,48 @@
+import 'dart:ui';
+
+class MyStyles {
+  MyStyles._();
+
+  /// FONT SIZE
+  static const double displayLarge = 96.0;
+  static const double displayMedium = 60.0;
+  static const double displaySmall = 48.0;
+  static const double headerMedium = 34.0;
+  static const double headerSmall = 24.0;
+  static const double titleLarge = 20.0;
+  static const double titleMedium = 16.0;
+  static const double titleSmall = 14.0;
+  static const double bodyLarge = 16.0;
+  static const double bodyMedium = 14.0;
+
+  /// Default text font size
+  static const double labelLarge = 14.0;
+
+  /// Default button font size
+  static const double bodySmall = 12.0;
+  static const double labelSmall = 10.0;
+
+  ///  FONT WEIGHT
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight normal = FontWeight.w400;
+  static const FontWeight bold = FontWeight.w700;
+
+  /// FONT SPACING
+  static const double displayLargeSpacing = -1.5;
+  static const double displayMediumSpacing = -0.5;
+  static const double displaySmallSpacing = 0.0;
+  static const double headerMediumSpacing = 0.25;
+  static const double headerSmallSpacing = 0.0;
+  static const double titleLargeSpacing = 0.15;
+  static const double titleMediumSpacing = 0.15;
+  static const double titleSmallSpacing = 0.1;
+  static const double bodyLargeSpacing = 0.5;
+  static const double bodyMediumSpacing = 0.25;
+
+  /// Default text font size
+  static const double labelLargeSpacing = 1.25;
+
+  /// Default button font size
+  static const double bodySmallSpacing = 0.4;
+  static const double labelSmallSpacing = 1.5;
+}

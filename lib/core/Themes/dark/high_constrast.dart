73e25daa4efdf1a294930a0d37af628d04f2d356
Legarde Montcho <tchioidriss@yahoo.fr>
2024-07-11 19:17:41 +0200

    Update high_constrast.dart

--- lib/core/Themes/dark/high_constrast.dart
+++ lib/core/Themes/dark/high_constrast.dart
@@ -1,1 +1,38 @@
-class ThemesDarkHighContrast {}
+import 'package:flutter/material.dart';
+
+
+class ThemesDarkHighContrast {
+  ThemesDarkHighContrast._();
+
+  /// DARK HIGH CONTRAST THEME
+  static final darkHighContrastTheme = ThemeData(
+    useMaterial3: false,
+    scaffoldBackgroundColor: TwConstants().colorDarkHighContrastMain,
+    primaryColor: TwConstants().colorDarkHighContrastPrimary,
+    indicatorColor: TwConstants().colorDarkHighContrastPrimary,
+    canvasColor: TwConstants().colorDarkHighContrastMutedMore,
+    cardColor: TwConstants().colorDarkHighContrastMutedMore,
+    dialogBackgroundColor: TwConstants().colorDarkHighContrastMutedMore,
+    disabledColor: TwConstants().colorDarkHighContrastUntouchable,
+
+    /// COLOR SCHEME
+    colorScheme: ColorScheme(
+      primary: TwConstants().colorDarkHighContrastPrimary,
+      inversePrimary: TwConstants().colorDarkHighContrastInverted,
+      secondary: TwConstants().colorDarkHighContrastPrimary,
+      secondaryContainer: TwConstants().colorDarkHighContrastPrimary,
+      surface: TwConstants().colorDarkHighContrastMain,
+      background: TwConstants().colorDarkHighContrastMain,
+      error: TwConstants().colorAlarmDark,
+      onPrimary: Colors.black,
+      onSecondary: Colors.white,
+      onSurface: Colors.white,
+      onBackground: Colors.white,
+      onError: Colors.black,
+      brightness: Brightness.dark,
+    ),
+
+   
+    /// ODERS
+  );
+}

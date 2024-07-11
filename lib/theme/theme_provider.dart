ec361353fbf11137ab87b5a12c8b0ef9ff669e4f
Legarde Montcho <tchioidriss@yahoo.fr>
2024-07-12 00:19:52 +0200

    Update theme_provider.dart

--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_folders_structure/core/utilities/preferences.dart';
 
-import '../locator.dart';
+import '../../../../core/_utilities.dart';
+import '../../../../locator.dart';
 
 class ThemeProvider extends ChangeNotifier {
   ThemeMode themeMode = ThemeMode.system;
 
   ThemeProvider() {
     String theme = sl.get<UtilityPreferences>().getString('Theme');
+    if (theme == 'Light') {
+      themeMode = ThemeMode.light;
+    } else if (theme == 'Dark') {
+      themeMode = ThemeMode.dark;
+    } else if (theme == 'System') {
+      themeMode = ThemeMode.system;
+    } else if (theme == '') {
+      themeMode = ThemeMode.dark;
+    }
+  }
+
+  bool get isDarkMode {
+    if (themeMode == ThemeMode.system) {
+      final brightness = WidgetsBinding.instance.platformDispatcher.platformBrightness;
+
+      return brightness == Brightness.dark;
+    }
+
+    return themeMode == ThemeMode.dark;
+  }
+
+  void changeTheme(ThemeMode themeModeIn) {
+    String themeModeString = '';
+    switch (themeModeIn) {
+      case ThemeMode.light:
+        {
+          themeModeString = 'Light';
+          themeMode = ThemeMode.light;
+        }
+        break;
+      case ThemeMode.dark:
+        {
+          themeModeString = 'Dark';
+          themeMode = ThemeMode.dark;
+        }
+        break;
+      case ThemeMode.system:
+        {
+          themeModeString = 'System';
+          themeMode = ThemeMode.system;
+        }
+        break;
+    }
+    sl.get<UtilityPreferences>().setString('Theme', themeModeString);
+    notifyListeners();
   }
 }

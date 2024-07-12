0f2bb7e50bfdd8f702bdea0831a3fcaa08a52af2
Legarde Montcho <tchioidriss@yahoo.fr>
2024-07-12 13:59:49 +0200

    Update breakpoints.dart

--- lib/responsive/domain/breakpoints.dart
+++ lib/responsive/domain/breakpoints.dart
@@ -1,7 +1,8 @@
-enum DeviceScreenType {
-  mobile,
-  tablet,
-  desktop,
-  largeDesktop,
-  wearable,
+class DeviceBreakpoints {
+  DeviceBreakpoints._();
+
+  static double get wearable => 218.0;
+  static double get tablet => 600.0;
+  static double get desktop => 801.0;
+  static double get largeDesktop => 1921.0;
 }

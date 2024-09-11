b301cf7af0a0cc89575c88d395e4483575f5fc6a
Drogbut <tchioidriss@yahoo.fr>
2024-09-11 16:14:54 +0200

    38 -add favoutite screen

--- lib/modules/shop/presenter/controllers/bottom_navigation/bottom_navigation.dart
+++ lib/modules/shop/presenter/controllers/bottom_navigation/bottom_navigation.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../ui/favourite/favourite_page.dart';
 import '../../ui/home/home.dart';
 import '../../ui/stores/store_page.dart';
 
 class BottomNavigationController extends GetxController {
   final Rx<int> selectedIndex = 0.obs;
 
-  final screens = [const HomePage(), const StorePage(), Container(), Container()];
+  final screens = [const HomePage(), const StorePage(), const WhishlistPage(), Container()];
 }

1f0ad744c243fef015f2aef43ac66690f250aa4e
Drogbut <tchioidriss@yahoo.fr>
2025-11-18 23:16:59 +0100

    no message

--- test/utilities/network_manager_mock.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_folders_structure/utilities/helpers/network_manager.dart';
-import 'package:mockito/annotations.dart';
-
-// Annotation pour générer le mock
-@GenerateMocks([NetworkManager])
-void main() {}
